01ce0d0bd881094613701b666fb63b726b6971df
Ruan Pereira Mattos <ruandupg@gmail.com>
2024-03-07 20:02:48 -0300

    Primeira versão

---
+++ src/main/java/com/ruanmattos/main/ExercicioWhile.java
@@ -0,0 +1,12 @@
+package com.ruanmattos.main;
+
+/**
+ *
+ * @author Ruan Pereira Mattos
+ */
+public class ExercicioWhile {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
