7ba59e0e3c95d184ae1d9adbb5e81ab17dd2451c
Ruan Pereira Mattos <ruandupg@gmail.com>
2024-03-07 20:07:06 -0300

    Primeira versão com as implementações feitas sobre os conhecimentos vistos em aula

--- src/main/java/com/ruanmattos/main/ExercicioWhile.java
+++ src/main/java/com/ruanmattos/main/ExercicioWhile.java
@@ -1,12 +1,26 @@
 package com.ruanmattos.main;
 
+import java.util.Scanner;
+
 /**
+ * Exercício sobre While
  *
  * @author Ruan Pereira Mattos
  */
 public class ExercicioWhile {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner s = new Scanner(System.in);
+        System.out.print("Digite algum número: ");
+        int x = s.nextInt();
+        int soma = 0;
+
+        while (x != 0) {
+            soma += x;
+            System.out.print("Digite algum número: ");
+            x = s.nextInt();
+        }
+        s.close();
+        System.out.println("A soma de todos os números é = " + soma);
     }
 }
