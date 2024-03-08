e910f41990165495a58bb03f9f42f25cde350db8
Ruan Pereira Mattos <ruandupg@gmail.com>
2024-03-08 19:24:02 -0300

    Melhoras e otimizações no código

--- src/main/java/com/ruanmattos/main/ExercicioWhile.java
+++ src/main/java/com/ruanmattos/main/ExercicioWhile.java
@@ -47,23 +47,44 @@ public class ExercicioWhile {
                     System.out.print("Digite a senha: ");
                     Scanner senha = new Scanner(System.in);
                     String senhaStr = senha.next();
-                    while (!"2002".equals(senhaStr)) {
-                        System.out.println("Senha Inválida");
-                        System.out.print("Digite a senha: ");
-                        senhaStr = senha.next();
+                    //Versão atual melhorada
+                    double senhasInvalidas = 0;
+                    double tentativas = 0;
+                    while (true) {
+                        tentativas++;
                         if (senhaStr.equals("2002")) {
                             System.out.println("Acesso Permitido");
+                            break;
+                        } else {
+                            senhasInvalidas++;
+                            System.out.println("Senha Inválida");
+                            System.out.print("Digite a senha: ");
+                            senhaStr = senha.next();
                         }
                     }
+                    if (senhasInvalidas == 0) {
+                        System.out.println("Você errour 0 vezes, dando um total de 100% de acerto");
+                    } else {
+                        double porcentagem = ((senhasInvalidas / tentativas) * 100);
+                        //System.out.println("Você errou " + senhasInvalidas + " vezes, dando um total de " + porcentagem + "% de acerto");
+                        System.out.printf("Você errou %.0f vezes, dando um total de %.2f de acerto\n", senhasInvalidas, porcentagem);
+                    }
+                    //Versão anterior
+                    /*while (!"2002".equals(senhaStr)) {
+                    System.out.println("Senha Inválida");
+                    System.out.print("Digite a senha: ");
+                    senhaStr = senha.next();
+                    }
+                    System.out.println("Acesso Permitido!");*/
                 }
                 case 3 -> {
                     System.out.println("Bem vindo(a) ao programa 3. Um Posto de combustíveis deseja determinar qual de seus produtos tem a preferência de seus clientes. Escreva um algoritmo para ler"
                             + " o tipo de combustível abastecido (codificado da seguinte forma: 1.Álcool 2.Gasolina 3.Diesel 4.Fim). Caso o usuário informe um código inválido (fora da faixa de 1 a 4)"
                             + " deve ser solicitado um novo código (até que seja válido). O programa será encerrado quando o código informado for o número 4. Deve ser escrito a mensagem: \"MUITO "
                             + "OBRIGADO\" e a quantidade de clientes que abasteceram cada tipo de combustível, conforme exemplo.");
-                    int alcool = 0;
-                    int gasolina = 0;
-                    int diesel = 0;
+                    float alcool = 0;
+                    float gasolina = 0;
+                    float diesel = 0;
                     Scanner combustivel = new Scanner(System.in);
                     System.out.println("1 - Álcool");
                     System.out.println("2 - Gasolina");
@@ -87,9 +108,16 @@ public class ExercicioWhile {
                         System.out.print("Digite o número de acordo com o combustível que você abasteceu: ");
                         escolhaCombustivel = combustivel.nextInt();
                     }
+                    float somaCombustivel = alcool + gasolina + diesel;
+                    float porcentagemAlcool = (alcool / somaCombustivel) * 100;
+                    float porcentagemGasolina = (gasolina / somaCombustivel) * 100;
+                    float porcentagemDiesel = (diesel / somaCombustivel) * 100;
                     System.out.println("Quantidade de clientes que abasteceram com Álcool: " + alcool);
+                    System.out.printf("Porcentagem de clientes que escolheram Álcool: %.2f\n", porcentagemAlcool);
                     System.out.println("Quantidade de clientes que abasteceram com Gasolina: " + gasolina);
+                    System.out.printf("Porcentagem de clientes que escolheram Gasolina: %.2f\n", porcentagemGasolina);
                     System.out.println("Quantidade de clientes que abasteceram com Diesel: " + diesel);
+                    System.out.printf("Porcentagem de clientes que escolheram Diesel: %.2f\n", porcentagemDiesel);
                 }
                 case 4 -> {
                     System.out.println("Bem vindo(a) ao programa 4. Imprimir a tabuada de um número");
